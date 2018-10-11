61d10183c9e3108a49475ea6fa499f6819d0df62
GMOHCM-Quinn <quinn@runsystem.net>
2018-10-11 17:09:53 +0700

    no message

--- XDemo.Android/Renderers/ExtendedElements/PullToRefreshLayoutRenderer.cs
+++ XDemo.Android/Renderers/ExtendedElements/PullToRefreshLayoutRenderer.cs
@@ -1,18 +1,4 @@
-﻿/*
- * Copyright (C) 2015 Refractored LLC & James Montemagno: 
- * http://github.com/JamesMontemagno
- * http://twitter.com/JamesMontemagno
- * http://refractored.com
- * 
- * The MIT License (MIT) see GitHub For more information
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Reflection;
 using Android.Runtime;
